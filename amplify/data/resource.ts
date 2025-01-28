f287c589408bec187cf67a666837e80387f23cf9
savagegeek22 <56052584+savagegeek22@users.noreply.github.com>
2025-01-27 22:19:00 -0500

    Added data isolation per user

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -11,7 +11,7 @@ const schema = a.schema({
     .model({
       content: a.string(),
     })
-    .authorization((allow) => [allow.publicApiKey()]),
+    .authorization((allow) => [allow.owner()]), // allow.owner was set to allow users to create their own to do lists
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,7 +19,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: "userPool", // userPool for Authorization was apiKey
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
